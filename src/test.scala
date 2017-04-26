aaf9e787f09fa21b879db510d1c5f93332ca171c
shutongliu <tong07@163.com>
2017-04-26 10:01:23 +0800

    1

---
+++ src/test.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by liust on 2017/4/19.
+ */
+class test {
+  def main (args: Array[String]){
+     if(args.length!=3){
+       print("11");
+       return
+     }
+    var sparkConf = new SparkConf()
+  }
+
+}
---
+++ src/test1.java
@@ -0,0 +1,5 @@
+/**
+ * Created by liust on 2017/4/26.
+ */
+public class test1 {
+}
